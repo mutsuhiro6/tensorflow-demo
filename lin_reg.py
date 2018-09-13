b7a93c2fdae5abec277c6b9a007573287ed3d739
Mutsuhiro Iwamoto <21per.nm@gmail.com>
2018-09-14 03:33:57 +0900

    Linear Regression with TensorFlow

---
+++ lin_reg.py
@@ -0,0 +1,20 @@
+import numpy as np
+import tensorflow as tf
+from sklearn.datasets import fetch_california_housing
+# import matplotlib.pyplot as plt
+
+
+housing = fetch_california_housing()
+m, n = housing.data.shape
+housing_data_plus_bias = np.c_[np.ones((m, 1)), housing.data]
+
+X = tf.constant(housing_data_plus_bias, dtype=tf.float32, name='X')
+y = tf.constant(housing.target.reshape(-1, 1), dtype=tf.float32, name='y')
+XT = tf.transpose(X)
+# theta = (X^T・X)^-1 ・X^T・y
+theta = tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(XT, X)), XT), y)
+
+with tf.Session() as sess:
+    theta_value = theta.eval()
+
+print(theta_value)
